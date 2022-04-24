b6501a95ac301b2f7674102a4d7218e96f0aecaf
Megan Lu <mklu847@gmail.com>
2022-04-23 21:54:14 -0500

    Initial Commit

---
+++ elementalPeriodicTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  elementalPeriodicTable
+//
+//  Created by Megan Lu on 4/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
